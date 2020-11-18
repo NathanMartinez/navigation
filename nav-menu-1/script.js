c5fc66d78575a5672445301ebcb8e53f90e35cba
Nate Martinez <natemartinez@Nates-iMac.local>
2020-11-18 13:41:00 -0700

    Added code to fix the issue that caused the mobile menu to be open if it was opened > resized to the desktop screen size then resized again to mobile. Now regardless if you dont manually close the menu it will auto close at 700 px

--- nav-menu-1/script.js
+++ nav-menu-1/script.js
@@ -6,3 +6,10 @@ const nav = document.getElementsByTagName('nav')[0]
 hamburgerMenu.addEventListener('click', e => {
   nav.classList.toggle('mobile-open')
 })
+
+window.addEventListener('resize', e => {
+  const width = e.target.innerWidth;
+  if (width >= 700) {
+    nav.classList.remove('mobile-open')
+  }
+})
